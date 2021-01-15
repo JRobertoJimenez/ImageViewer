bade16f2539ab95d49dd4baf5b84ea1f6d2a2e98
José Roberto Jiménez <Jos Roberto Jimnez@PC-JRJR>
2021-01-15 21:07:50 +0000

    ImageViewer version 1.01

---
+++ src/ui/SwingImageDisplay.java
@@ -0,0 +1,71 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import model.Image;
+
+public class SwingImageDisplay  extends JPanel implements ImageDisplay {
+    private Image currentImage;
+
+
+    
+    @Override
+    public Image current() {
+        return currentImage;
+    }
+
+    @Override
+    public void show(Image image) {
+        this.currentImage = image;
+        this.repaint();
+        
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        if (currentImage == null) return;
+        g.clearRect(0, 0, this.getWidth(), this.getHeight());
+        BufferedImage bi=imageOf(currentImage);
+        
+        double [] d=getD(bi);
+        int w=(int)d[0];
+        int h=(int)d[1];
+        
+        g.drawImage(bi, (this.getWidth()-w)/2, (this.getHeight()-h)/2,
+        (this.getWidth()-w)/2+w, (this.getHeight()-h)/2+h,
+        0, 0,
+        bi.getWidth(),bi.getHeight(), null);
+    }
+
+    private BufferedImage imageOf(Image image) {
+        try {
+            BufferedImage bi=ImageIO.read(image.stream());
+            return bi;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    };
+
+    private double[] getD(BufferedImage bi) {
+        double [] res= {bi.getTileWidth(),bi.getHeight()};
+        if(res[0]>this.getWidth()){
+            res[0]=this.getWidth();
+            res[1]=(res[0]/bi.getWidth())*bi.getHeight();
+        }
+        if(res[1]>this.getHeight()){
+            res[1]=this.getHeight();
+            res[0]=(res[1]/bi.getHeight())*bi.getWidth();
+        }
+        
+        return res;
+    }
+
+    
+
+}
